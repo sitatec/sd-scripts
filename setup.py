7d7264a95f808cdbfe8af2af67aae507af870362
Sita Bérété <sita.berete.3@gmail.com>
2023-02-12 21:33:24 +0000

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,16 @@
-from setuptools import setup, find_packages
+import os
+
+import pkg_resources
+from setuptools import find_packages, setup
  
-setup(name = "library", packages = find_packages())
+setup(
+  name = "sdscripts", 
+  packages = find_packages(),
+  version= "0.0.1",
+  install_requires=[
+    str(r)
+    for r in pkg_resources.parse_requirements(
+      open(os.path.join(os.path.dirname(__file__), "requirements.txt"))
+    )
+  ],
+)
